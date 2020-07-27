b3fe6863bc80e5caddcae3b4a5e06a6a9054ec57
Niranjan <niranjan@localhost.localdomain>
2020-07-27 23:26:43 +0530

    pylint overviewed

--- dailycodingproblem_1.py
+++ dailycodingproblem_1.py
@@ -1,19 +1,20 @@
-#This problem was recently asked by Google.
-#Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
-#For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+'''
+This problem was recently asked by Google.
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+'''
 import time
-l=input("Enter the list:").split()
-j=int(input('enter the key:'))
-for i in range(len(l)):
-    l[i]=int(l[i])
-for k in l:
-    for m in l:
-        if k+m==j:
-            print('True')
+L = input("Enter the list:").split()
+J = int(input('Enter the key:'))
+for i in enumerate(L):
+    L[i] = int(i[1])
+for k in enumerate(L):
+    for m in enumerate(L):
+        if k[1]+m[1] == J:
+            print 'True'
             time.sleep(3)
             exit()
         else:
-            n=1
-if n==1:
-    print('False')
-    
+            n = 1
+if n == 1:
+    print 'False'
